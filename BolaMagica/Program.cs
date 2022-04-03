cd754c527fd30981ec7ba60a43909d9245003270
LeaRamos <temple805@gmail.com>
2022-04-03 20:34:58 -0300

    ejercicio bolaMagica

---
+++ BolaMagica/Program.cs
@@ -0,0 +1,27 @@
+﻿using PW3_2022_1C_Clase_1.Entidades;
+using System;
+
+namespace BolaMagica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lista=MagicBall.listaDeRespuestas;
+            
+
+            Console.WriteLine("Bienvenido a la Bola Magica");
+            Console.WriteLine("Escriba una pregunta que desee ser respondida");
+            Console.WriteLine("La pregunta debe ser de respuesta de SI/NO");
+
+            Console.WriteLine("Tu pregunta: ");
+            String pregunta = Console.ReadLine();
+
+           
+            String respuesta = MagicBall.devolverRespuesta(lista);
+
+            Console.WriteLine("Respuesta: " + respuesta);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PW3-2022-1C-Clase-1.Entidades/MagicBall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW3_2022_1C_Clase_1.Entidades
+{
+    public static class MagicBall
+    {
+        static String[] _listaDeRespuestas = { "Es cierto", "Es decididamente as", "Sin lugar a dudas", "Sí, definitivamente", "Puedes confiar en ello",
+                                        "Como yo lo veo, sí", "Lo más probable","Perspectiva buena","Sí" , "Las señales apuntan a que sí",
+                                        "Respuesta confusa, vuelve a intentarlo", "Vuelve a preguntar más tarde", "Mejor no decirte ahora","No se puede predecir ahora"," Concéntrate y vuelve a preguntar",
+                                         "No cuentes con ello","Mi respuesta es no","Mis fuentes dicen que no","Las perspectivas no son muy buenas","Muy dudoso"};
+
+        public static String devolverRespuesta(String[] lista) {
+            Random random = new Random();
+            Int64 numeroRandom = random.Next(0, 19);
+            String respuesta = lista[numeroRandom];
+            return respuesta;
+        }
+
+
+        public static string[] listaDeRespuestas
+        {
+            get => _listaDeRespuestas;
+            set => _listaDeRespuestas = value;
+        }
+
+    }
+}
